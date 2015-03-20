f9677272ffbecdf02c129c49d163b9b1ab0e43a4
patrioticcow <cristi.catea@gmail.com>
2015-03-20 02:44:34 -0700

    0.5.2.4

--- dist/win32/MessagesForSkype/Resources/app.js
+++ dist/win32/MessagesForSkype/Resources/app.js
@@ -190,8 +190,6 @@ function getPath() {
 }
 
 function insertDefaults(path) {
-	alert(path);	alert("INSERT INTO container (id, skypePath) VALUES (1, '" + path + "')");
-
 	var db = Ti.Database.openFile(Ti.Filesystem.getFile(Ti.Filesystem.getApplicationDataDirectory(), 'database.db'));
 	db.execute("INSERT INTO container (id, skypePath) VALUES (1, '" + path + "')");
 	alert('The path was saved successfully');
